0e700afb8150a707599f12e6e25c2bd0e3537dc8
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2024-12-30 23:39:03 +0100

    Update containsDuplicate.js

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,1 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const uniqueItems = new Map()
 
+    for (let i = 0; i < nums.length; i++) {
+        if (uniqueItems.get(nums[i])) {
+            return true
+        }
+        uniqueItems.set(nums[i], 1)
+    }
+
+    return false
+};
