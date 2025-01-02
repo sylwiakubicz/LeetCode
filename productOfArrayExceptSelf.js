466522908aad2f66eeba8a78b8e47274bdd66e26
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-02 22:16:59 +0100

    Update productOfArrayExceptSelf.js

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -1,1 +1,19 @@
+// niezoptymalizowany pamięciowo
+var productExceptSelf = function(nums) {
+    const n = nums.length
+    let result = new Array(n)
+    let prefixArray = new Array(n)
+    prefixArray[0] = 1
+    let suffixArray = new Array(n)
+    suffixArray[nums.length - 1] = 1
 
+    for (let i = n - 2; i >= 0; i--) {
+        suffixArray[i] = suffixArray[i + 1] * nums[i + 1]
+    }
+    for (let i = 1; i < n; i++) {
+        prefixArray[i] = prefixArray[i - 1] * nums[i - 1]
+        result[i - 1] = prefixArray[i - 1] * suffixArray[i - 1]
+    }
+    result[n - 1] = prefixArray[n - 1] * suffixArray[n - 1]
+    return result
+};
