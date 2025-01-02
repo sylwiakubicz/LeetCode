a455d183dbf4e767f57d7738ce614a667482e433
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-02 17:58:30 +0100

    Update topKFrequent.js

--- topKFrequent.js
+++ topKFrequent.js
@@ -1,1 +1,16 @@
+var topKFrequent = function(nums, k) {
+    let hashMap = new Map()
+    let topFrequnetNumber = new Array()
 
+    for (let i = 0; i < nums.length; i++) {
+        hashMap.set(nums[i], (hashMap.get(nums[i]) || 0) + 1)
+    }  
+
+    let sortedNumsArray = Array.from(hashMap).sort((a,b) => b[1] - a[1])
+
+    for (let j = 0; j < k; j++) {
+        topFrequnetNumber.push(sortedNumsArray[j][0])
+    } 
+    return topFrequnetNumber
+
+};
