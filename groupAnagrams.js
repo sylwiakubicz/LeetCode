bc2198f322beb163ced9a2ce1e8f03769c685f2d
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-02 15:49:43 +0100

    Update groupAnagrams.js

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,1 +1,15 @@
 
+var groupAnagrams = function(strs) {
+
+    let anagrams = new Map()
+    
+    for (let i = 0; i < strs.length; i++) {
+        let key = Array.from(strs[i]).sort().join('')
+        if (anagrams.has(key)) {
+            let newVal = anagrams.get(key).push(strs[i])
+            continue
+        }
+        anagrams.set(key, [strs[i]])
+    }
+    return Array.from(anagrams.values());
+};
