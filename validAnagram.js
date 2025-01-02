ae8b08abf97126abe01f343ac71caef4caa44e32
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-02 14:05:32 +0100

    Update validAnagram.js

--- validAnagram.js
+++ validAnagram.js
@@ -1,1 +1,19 @@
+var isAnagram = function(s, t) {
 
+    if (s.length != t.length) return false
+
+    let sLetters = new Map()
+    let tLetters = new Map()
+
+    for  (let i = 0; i < s.length; i++) {
+        sLetters.set(s[i], (sLetters.get(s[i]) || 0) + 1)
+        tLetters.set(t[i], (tLetters.get(t[i]) || 0) + 1)
+    }
+
+    for (let [key, value] of sLetters) {
+        if (!tLetters.has(key)) return false
+        if (tLetters.get(key) != value) return false
+    }
+
+    return true
+};
