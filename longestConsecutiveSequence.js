3473636f7e273fa66997ef6cd467f4d8ab7dbeda
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-03 22:20:16 +0100

    Update longestConsecutiveSequence.js

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,1 +1,29 @@
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) return 0
 
+    let set = new Set()
+    let maxSequence = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) continue
+        set.add(nums[i])
+    }
+
+    for (let val of set) {
+        let i = 1
+        let count = 1
+        while (set.has(val - i)) {
+            set.delete(val - i)
+            count++
+            i++
+        }
+        let j = 1
+        while (set.has(val + j)) {
+            set.delete(val + j)
+            count++
+            j++
+        }
+        if (maxSequence < count) maxSequence = count
+    }
+    return maxSequence
+};
