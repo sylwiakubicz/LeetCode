77c70454e3db3cad9fefd2861ea6d52f3867be24
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-03 19:45:17 +0100

    Update validSudoku.js

--- validSudoku.js
+++ validSudoku.js
@@ -32,3 +32,31 @@ var isValidSudoku = function(board) {
     }
     return true
 };
+
+// trochę bardziej zoptymalizowany bez zagnieżdżannia ifów
+var isValidSudoku = function(board) {
+
+
+    for (let i = 0; i < 9; i++) {
+        let row = new Set()
+        let column = new Set()
+        let box = new Set()
+
+        for (let j = 0; j < 9; j++) {
+            
+            if (board[i][j] != "." && row.has(board[i][j])) return false
+            row.add(board[i][j])
+            
+            
+            if (board[j][i] != "." && column.has(board[j][i])) return false
+            column.add(board[j][i])
+            
+
+            let boxRow = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+            let boxCol = 3 * (i % 3) + (j % 3);
+            if (board[boxRow][boxCol] != "." && box.has(board[boxRow][boxCol])) return false
+            box.add(board[boxRow][boxCol])
+        }
+    }
+    return true
+};
