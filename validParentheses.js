fbfb0c715dc987eb342c50953317485ad58e3456
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-04 11:22:32 +0100

    Update validParentheses.js

--- validParentheses.js
+++ validParentheses.js
@@ -1,1 +1,29 @@
+var isValid = function(s) {
+    let parentheses = new Array()
 
+    for (let char of s) {
+        if (char === "(" || char === "{" || char === "[") {
+            parentheses.push(char)
+            continue
+        }
+
+        let currentTop = parentheses[parentheses.length - 1]
+        if (char === "}" && currentTop !== "{") {
+            return false
+        }
+        if (char === ")" && currentTop !== "(") {
+            return false
+        }
+        if (char === "]" && currentTop !== "[") {
+            return false
+        }
+
+        parentheses.pop(currentTop)
+    }
+
+    if (parentheses.length !== 0) {
+        return false
+    }
+    return true
+
+};
