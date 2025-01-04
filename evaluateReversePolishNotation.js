84926c2bb72cf5795e4bdc8a7d6f1a5085ccd241
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-04 23:02:14 +0100

    Update evaluateReversePolishNotation.js

--- evaluateReversePolishNotation.js
+++ evaluateReversePolishNotation.js
@@ -1,1 +1,46 @@
+// trochę brzydka zapisowo notacja
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
 
+    let numbers = new Array()
+
+    for (let i = 0; i < tokens.length; i++) {
+        let num = parseInt(tokens[i])
+
+        if (num || num === 0) {
+            numbers.push(num)
+            continue
+        }
+        
+        let a = numbers.pop(numbers.length - 1)
+        let b = numbers.pop(numbers.length - 1)
+        let operation = tokens[i]
+        let result = calculate(a,b,operation)
+                
+        numbers.push(result)
+    }
+
+    return numbers[0]
+};
+
+var calculate = function(a,b,operation) {
+    let result;
+    switch (operation) {
+        case "+": 
+            result = b + a
+            break
+        case "-": 
+            result = b - a
+            break
+        case "*":
+            result = b * a
+            break
+        case "/":
+            result = (b / a) < 0 ? Math.ceil(b / a) : Math.floor(b / a)
+            break
+    }
+    return result
+}
