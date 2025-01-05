8152f59e6cfa06f8d9a5ddde68b4bfcb2bfb21f2
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-05 22:25:11 +0100

    Update dailyTemperatures.js

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -15,3 +15,25 @@ var dailyTemperatures = function(temperatures) {
 
     return days
 };
+
+
+// rozwiązania bardziej optymalne przechowywanie tylko indeksów, nie musimy trzymać całej tabeli z temperaturami
+
+var dailyTemperatures = function(temperatures) {
+    let arrLength = temperatures.length;
+    let outputArr = new Array(arrLength).fill(0);
+    let helperArr = [];
+    for (let i= arrLength-1; i>=0; i--){
+      while(helperArr.length && temperatures[i] >= temperatures[helperArr[helperArr.length-1]]) {
+            helperArr.pop();
+      }
+
+      if(helperArr.length){
+        outputArr[i] = helperArr[helperArr.length-1] - i;
+      }
+
+      helperArr.push(i);
+    }
+
+    return outputArr;
+};
