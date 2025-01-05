d44ecee5e712c8468b653d94d43d5e642b1d68e5
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-05 22:24:17 +0100

    Update dailyTemperatures.js

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -1,1 +1,17 @@
+var dailyTemperatures = function(temperatures) {
+    let days = new Array(temperatures.length)
+    let stack = []
 
+    for (let i = 0; i < temperatures.length; i++) {
+        while (stack.length !== 0 && stack[stack.length - 1][0] < temperatures[i]) {
+            temp = stack.pop()
+            tempIndex = temp[1]
+            days[tempIndex] = i - tempIndex
+        }
+
+        stack.push([temperatures[i], i])
+        days[i] = 0
+    }
+
+    return days
+};
