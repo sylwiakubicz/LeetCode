c9a888381a5c48a1b8e507465c1845faf8405133
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-06 22:18:54 +0100

    Update generateParenttheses.js

--- generateParenttheses.js
+++ generateParenttheses.js
@@ -1,1 +1,29 @@
 
+var generateParenthesis = function(n) {
+    let strings = []
+    let stack = []
+
+    function backtrack(opened, closed) {
+        if (n === opened && opened === closed) {
+            strings.push(stack.join(""))
+            return
+        }
+
+        if (opened < n) {
+            stack.push("(")
+            backtrack(opened + 1, closed)
+            stack.pop()
+        }
+
+        if (closed < opened) {
+            stack.push(")")
+            backtrack(opened, closed + 1)
+            stack.pop()
+        }
+    }
+
+    backtrack(0,0)
+
+    return strings
+};
+
