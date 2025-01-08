5f9ddcf3cfda23cce77facb941b1dc7ea550c6c9
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-08 23:33:38 +0100

    Update largestRectangleInHisogram.js

--- largestRectangleInHisogram.js
+++ largestRectangleInHisogram.js
@@ -1,1 +1,29 @@
+var largestRectangleArea = function(heights) {
+    let stack = []
+    let maxArea = 0
+    
+    for (let i = 0; i < heights.length; i++) {
+        let start = i
+        while (stack.length !== 0 && stack[stack.length - 1][1] > heights[i]) {
+            [prevIndex, prevHeight] = stack.pop()
+            start = prevIndex
+            if (maxArea < (prevHeight * (i - prevIndex))) {
+                maxArea = prevHeight * (i - prevIndex)
+            }
 
+        }
+
+        stack.push([start, heights[i]])
+
+    }
+
+    while (stack.length !== 0) {
+        [prevIndex, prevHeight] = stack.pop()
+        if (maxArea < (prevHeight * (heights.length - prevIndex))) {
+            maxArea = prevHeight * (heights.length - prevIndex)
+        }
+    }
+
+
+    return maxArea
+};
