cf69c7490701be8dd61baaa6180be020ebe5d8b6
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-09 19:40:35 +0100

    Update validPalindrome.js

--- validPalindrome.js
+++ validPalindrome.js
@@ -1,1 +1,25 @@
+var isPalindrome = function(s) {
+    let sConverted = ""
+    sLowercase = s.toLowerCase()
 
+    for (let char of sLowercase) {
+        if ((char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) || (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57)) {
+            sConverted += char
+        }
+    }
+
+    if (!sConverted) return true
+
+    let i = 0
+    let j = sConverted.length - 1
+    while (sConverted[i] === sConverted[j] && i < j) {
+        i++
+        j--
+    }
+
+    if (j - i === 0 || i - j === 1) {
+        return true
+    }
+
+    return false
+};
