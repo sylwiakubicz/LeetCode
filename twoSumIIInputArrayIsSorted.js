c5e99d000a810adb954b5831bb2b40efd32147b3
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-09 21:58:29 +0100

    Update twoSumIIInputArrayIsSorted.js

--- twoSumIIInputArrayIsSorted.js
+++ twoSumIIInputArrayIsSorted.js
@@ -1,1 +1,17 @@
+// Z wykorzysataniem two pinters ale tutaj mamy praktycznie o(n2) bo za każdym razem musimy resetować j
+var twoSum = function(numbers, target) {
 
+    for (let i = 0; i < numbers.length; i++) {
+        let j = numbers.length - 1
+
+        let x = target - numbers[i]
+        if (x < numbers[i]) continue
+
+        while (numbers[j] >= x) {
+            if (numbers[j] === x) return [i + 1, j + 1]
+            j--
+        }
+    }
+
+    return []
+};
