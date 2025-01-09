c870d6b2d4711a1f78ca54f8a1556023e01bf43c
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-09 22:29:44 +0100

    Update twoSumIIInputArrayIsSorted.js

--- twoSumIIInputArrayIsSorted.js
+++ twoSumIIInputArrayIsSorted.js
@@ -15,3 +15,39 @@ var twoSum = function(numbers, target) {
 
     return []
 };
+
+// wersja z binary search zamiast dodatkowej pętli while 
+var twoSum = function(numbers, target) {
+    
+    for (let i = 0; i < numbers.length; i++) {
+        let x = target - numbers[i]
+        if (x < numbers[i]) continue
+
+        let result = binarySearch(numbers, x, i)
+        if (result === null) continue
+
+        return [i + 1, result + 1]
+    }
+
+    return []
+};
+
+var binarySearch = function(array, target, currentI) {
+    let left = 0
+    let right = array.length - 1
+    let middle = 0
+    
+    while (left <= right) {
+        middle = Math.floor((right + left) / 2)
+
+        if (array[middle] === target && middle !== currentI) return middle
+        if (array[middle] <= target) {
+            left = middle + 1
+        }
+        else {
+            right = middle - 1
+        }
+    }
+
+    return null
+}
