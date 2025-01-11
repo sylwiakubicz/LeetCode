68eee40c453556cd5dbf19f476df998595585aa4
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-11 12:07:41 +0100

    Update containerithMostWater.js

--- containerithMostWater.js
+++ containerithMostWater.js
@@ -1,1 +1,21 @@
+var maxArea = function(height) {
+    let maxArea = 0
+    let currentArea = 0
+    let i = 0
+    let j = height.length - 1
 
+    while (j > i) {
+        if (height[i] > height[j]) {
+            currentArea = height[j] * (j - i)
+            j--
+        } else {
+            currentArea = height[i] * (j - i)
+            i++
+        }
+
+        if (currentArea > maxArea) {
+            maxArea = currentArea
+        }
+    }
+    return maxArea
+};
