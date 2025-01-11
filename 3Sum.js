ccfeb3749f8561b1f85084ddd1471339c5d65bd8
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-11 16:27:40 +0100

    Update 3Sum.js

--- 3Sum.js
+++ 3Sum.js
@@ -37,3 +37,38 @@ var threeSum = function(nums) {
 
     return Array.from(solutions.values())
 }
+
+// rozwiązanie bez sprawdzania już dodanych opcji
+
+var threeSum = function(nums) {
+    const numsSort = [...nums].sort((a,b) => a-b);
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && numsSort[i] === numsSort[i - 1]) continue;
+
+        let left = i + 1;
+        let right = numsSort.length - 1;
+
+        while(left < right) {
+            const summ = numsSort[i] + numsSort[left] + numsSort[right];
+
+            if (summ === 0) {
+                result.push([numsSort[i], numsSort[left], numsSort[right]]);
+
+                while (left < right && numsSort[left] === numsSort[left + 1]) left++;
+                while (left < right && numsSort[right] === numsSort[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (summ > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+    }
+
+    return result;
+};
