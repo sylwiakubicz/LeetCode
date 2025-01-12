831c71f0f9c7d32b048a152d28962115b412d74b
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-12 16:04:57 +0100

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,1 +1,20 @@
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    let middle = 0
 
+    while (left <= right) {
+        middle = Math.floor((left + right) / 2)
+        
+        if (nums[middle] === target) return middle
+        
+        if (nums[middle] > target) {
+            right = middle - 1
+        }
+        else {
+            left = middle + 1
+        }
+    }
+
+    return -1
+};
