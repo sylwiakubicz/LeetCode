41a9774e883246a9dc73ce0985fc2442b7afc147
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-13 22:54:10 +0100

    Update findMinimumInRotatedSortedArray.js

--- findMinimumInRotatedSortedArray.js
+++ findMinimumInRotatedSortedArray.js
@@ -1,1 +1,50 @@
+var findMin = function(nums) {
+    let l = 0
+    let r = nums.length - 1
+    let m = 0
+    let min = nums[m]
 
+    while (l <= r) {
+        m = Math.floor((l + r) / 2)
+        
+        if (nums[m] < min) {
+            min = nums[m]
+        }
+
+
+        if (nums[m - 1] > nums[m + 1]) {
+            if (nums[l] > nums[r]) {
+                if (nums[m + 1] <= nums[r]) {
+                    l = m + 1
+                } else {
+                    r = m - 1
+                }
+
+            } else  {
+                if (nums[m + 1] <= nums[l]) {
+                    l = m + 1
+                } else {
+                    r = m - 1
+                }
+            }
+        } else  {
+            if (nums[l] > nums[r]) {
+                if (nums[r] >= nums[m - 1]) {
+                    r = m - 1
+                }
+                else {
+                    l = m + 1
+                }
+
+            } else {
+                if (nums[l] <= nums[m - 1]) {
+                    r = m - 1
+                } else {
+                    l = m + 1
+                }
+            }
+        }
+    }
+
+    return min
+};
