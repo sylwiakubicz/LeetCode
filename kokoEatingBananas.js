85e1b612f6468ac71f3c4e419724a51d7827b83e
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-13 19:57:44 +0100

    Update kokoEatingBananas.js

--- kokoEatingBananas.js
+++ kokoEatingBananas.js
@@ -1,1 +1,31 @@
+var minEatingSpeed = function(piles, h) {
+    let maxPile = Math.max(...piles)
+    if (piles.length === h) return maxPile
 
+    let left = 1
+    let right = maxPile
+    let middle = 0
+    let result = 0
+    let k = maxPile
+
+    while (left <= right) {
+        middle = Math.floor((left + right) / 2)
+        result = 0
+
+        for (let i = 0; i < piles.length; i++) {
+            result += Math.ceil(piles[i] / middle)
+        }
+
+        if (k > middle && result <= h) {
+            k = middle
+        }
+
+        if (result <= h) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+
+    return k
+};
