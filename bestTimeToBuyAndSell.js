726c09d90b1d9e6c77927f9b27eb2d373ebf0997
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-18 16:16:05 +0100

    Update bestTimeToBuyAndSell.js

--- bestTimeToBuyAndSell.js
+++ bestTimeToBuyAndSell.js
@@ -1,1 +1,19 @@
 
+var maxProfit = function(prices) {
+    let maxProfit = 0
+    let currentMin = prices[0]
+
+    for (let i = 1; i < prices.length; i++) {
+        profit = prices[i] - currentMin
+
+        if (profit > maxProfit) {
+            maxProfit = profit
+        }
+        
+        if (currentMin > prices[i]) {
+            currentMin = prices[i]
+        }
+    }  
+
+    return maxProfit
+};
