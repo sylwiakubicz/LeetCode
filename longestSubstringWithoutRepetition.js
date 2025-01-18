4ce1a8905c0753691d94dd4f2124f49d417f6cad
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-18 17:56:22 +0100

    Update longestSubstringWithoutRepetition.js

--- longestSubstringWithoutRepetition.js
+++ longestSubstringWithoutRepetition.js
@@ -1,1 +1,34 @@
+var lengthOfLongestSubstring = function(s) {
+    if (s.length === 0) return 0
+    
+    let longestSubstring = 0
+    let chars = new Map()
+    chars.set(s[0], 0)
+    
+    let l = 0
+    let r = 1
 
+    while (r < s.length) {
+
+        if (chars.has(s[r])) {
+            longestSubstring = Math.max(chars.size, longestSubstring)
+            idx = chars.get(s[r])
+            
+            while (l <= idx) {
+                chars.delete(s[l])
+                l++
+            }
+
+            if (chars.has(s[l], l)) continue
+            chars.set(s[l], l)
+
+        } else {
+            chars.set(s[r], r)
+        }
+        
+        r++
+    }
+
+    longestSubstring = Math.max(chars.size, longestSubstring)
+    return longestSubstring
+};
