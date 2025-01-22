5f58dd9f1642e5cffea55cd3230b68f3ea40ce97
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-22 22:05:59 +0100

    Update validAnagram.js

--- validAnagram.js
+++ validAnagram.js
@@ -1,3 +1,24 @@
+// wykorzystanie tablicy o stałej wartości
+// mniej zajmowanej pamieci -> tablica a nie mapa z kluczem i wartością
+ var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
+
+    let sCharsArray = new Array(26).fill(0)
+    let tCharsArray = new Array(26).fill(0)
+
+    for (let i = 0; i < s.length; i++) {
+        sCharsArray[s.charCodeAt(i) - 97]++
+        tCharsArray[t.charCodeAt(i) - 97]++
+    }
+
+    for (let i = 0; i <= 26; i++) {
+        if (sCharsArray[i] !== tCharsArray[i]) return false
+    }
+
+    return true
+};
+
+// Działa sprawnie, dodajemy litery do hashmapy i póżniej porównujemy wartości między dwoma mapami
 var isAnagram = function(s, t) {
 
     if (s.length != t.length) return false
