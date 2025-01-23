bcb10e2da5029e251f6cd7e0e17dcc0b3390433e
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-23 22:22:07 +0100

    Update slidingWindowMaximum.js

--- slidingWindowMaximum.js
+++ slidingWindowMaximum.js
@@ -1,3 +1,30 @@
+// O(n) 
+var maxSlidingWindow = function(nums, k) {
+    let n = nums.length
+    let output = []
+    let q = new Deque()
+    let l = 0
+    let r = 0
+
+    while (r < n) {
+        while (q.size() && nums[q.back()] < nums[r]) {
+            q.popBack()
+        }
+        q.pushBack(r)
+
+        if (l > q.front()) {
+            q.popFront()
+        }
+
+        if ((r + 1) >= k) {
+            output[l] = nums[q.front()]
+            l++
+        }
+        r++
+    }   
+    return output
+};
+
 // działa poprawnie ale nie jest za bardzo optymalny
 var maxSlidingWindow = function(nums, k) {
     let res = []
