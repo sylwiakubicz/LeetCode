17a308c12f1bc06653ad87acbe703f9a7ccac492
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-25 12:14:36 +0100

    Update linkedListCycle.js

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,1 +1,14 @@
+var hasCycle = function(head) {
+    if (!head) return false
 
+    let slow = head
+    let fast = head.next
+
+    while (fast !== null && fast.next !== null) {
+        if (fast === slow) return true
+        fast = fast.next.next
+        slow = slow.next
+    }
+
+    return false
+};
