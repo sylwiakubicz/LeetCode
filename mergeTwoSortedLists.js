758ad933982a2b7a3fc32d0c8a93ec6928554bb0
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-25 16:44:50 +0100

    Update mergeTwoSortedLists.js

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -1,3 +1,29 @@
+// Iteracyjne rozwiązanie
+var mergeTwoLists = function(list1, list2) {
+    let dummy = {val: 0, next: null}
+    let node = dummy
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            node.next = list1
+            list1 = list1.next
+        } else {
+            node.next = list2
+            list2 = list2.next
+        }
+        node = node.next
+    }
+
+    if (list1) {
+        node.next = list1
+    } else if (list2) {
+        node.next = list2
+    }
+
+    // dummy.next to początkowy węzeł listy, a dzięki strukturze next tworzymy pełną, jednokierunkową listę. 
+    return dummy.next
+};
+
 // rekurencyjne rozwiązanie
 var mergeTwoLists = function(list1, list2) {
     if (!list1) return list2
