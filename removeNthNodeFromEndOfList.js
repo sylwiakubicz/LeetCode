b4bdc33d4047d9f987062f489ef8b37a99de7725
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-26 10:55:25 +0100

    Update removeNthNodeFromEndOfList.js

--- removeNthNodeFromEndOfList.js
+++ removeNthNodeFromEndOfList.js
@@ -1,7 +1,10 @@
+// nie pytali o reverse wiec niekoniecznie chcemy odwracać naszą listę i usuwać node gdy n = 2
 var removeNthFromEnd = function(head, n) {
+    // tworzymy dummy node, do którego będziemy następnie przyłączać pozostałe
     let dummy = {val: 0, next: null}
     let node = dummy
-    
+
+    // sprawdzamy size naszej linked listy 
     let size = 0
     let current = head
     while (current) {
@@ -9,16 +12,23 @@ var removeNthFromEnd = function(head, n) {
         current = current.next
     }
 
+    // current musi wrócić na początek linked listy bo zatrzymałs ię na wskazywaniu na null przez wcześńiejszą pętle
     current = head
+    // dzięki size wiemy ile początkowych elementów zostanie bez zmian zanim coś będziemy chceicli usunąć size - n
     let i = size - n
+    // dopóki current przypisujemy do node kolejne elementy i zmiejszamy i o 1 dopóki nie równa się 0
     while (current) {
         if (i === 0) {
+            // dla i = 0 oznacza że dotarliśmy do noda, który ma byc usunięty więc przed dodaniem go do listy przeuswamy current o jeden do przodu i to jego przypisujemyt
             current = current.next
             node.next = current
             node = node.next
+            // możemy zrobić break bo current zawiera wskaźniki do końca listy wiec całą końcówkę już mamy dodaną 
             break
         } 
+        // przypisujemt current do nnoda
         node.next = current
+        // przesuwamy się o jeden node w prawo na obu listach
         current = current.next
         node = node.next
         i--
