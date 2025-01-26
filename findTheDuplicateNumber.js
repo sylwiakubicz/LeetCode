3f498423294b8a76ff06a84e6069e22df3d138c8
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-26 14:56:45 +0100

    Update findTheDuplicateNumber.js

--- findTheDuplicateNumber.js
+++ findTheDuplicateNumber.js
@@ -1,3 +1,25 @@
+// Floyd's algorym 
+var findDuplicate = function(nums) {
+    let slow = 0
+    let fast = 0
+    while (true) {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if (slow === fast) {
+            break
+        }
+    }
+
+    let slow2 = 0
+    while (true) {
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if (slow === slow2) {
+            return slow
+        }
+    }
+}
+
 // brute force
 var findDuplicate = function(nums) {
     for (let i = 0; i < nums.length; i++) {
