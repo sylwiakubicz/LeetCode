1c30f127c515b3b53dfb3ce1a531adfa6c2b78af
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-26 14:16:13 +0100

    Update findTheDuplicateNumber.js

--- findTheDuplicateNumber.js
+++ findTheDuplicateNumber.js
@@ -1,1 +1,8 @@
-
+// brute force
+var findDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) return nums[i]
+        }
+    }
+};
