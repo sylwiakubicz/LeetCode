c968d9db47506f99d3242e642a4756a74680d149
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-26 10:41:56 +0100

    Update removeNthNodeFromEndOfList.js

--- removeNthNodeFromEndOfList.js
+++ removeNthNodeFromEndOfList.js
@@ -1,1 +1,28 @@
+var removeNthFromEnd = function(head, n) {
+    let dummy = {val: 0, next: null}
+    let node = dummy
+    
+    let size = 0
+    let current = head
+    while (current) {
+        size++
+        current = current.next
+    }
 
+    current = head
+    let i = size - n
+    while (current) {
+        if (i === 0) {
+            current = current.next
+            node.next = current
+            node = node.next
+            break
+        } 
+        node.next = current
+        current = current.next
+        node = node.next
+        i--
+    }
+
+    return dummy.next
+};
