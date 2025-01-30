f3b79002dcc68481429a4c939d0259276e3af716
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-01-30 22:35:41 +0100

    Update maximumDepthOfBinaryTree.js

--- maximumDepthOfBinaryTree.js
+++ maximumDepthOfBinaryTree.js
@@ -4,3 +4,28 @@ var maxDepth = function(root) {
     if (root === null) return 0
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
+
+
+// bfs
+
+var maxDepth = function(root) {
+    if (root === null) return 0
+    
+    let result = 0
+    let queue = [root] 
+    while (queue.length > 0) {
+        let size = queue.length
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift()
+            if (node.left !== null) {
+                queue.push(node.left)
+            }
+            if (node.right !== null) {
+                queue.push(node.right)
+            }
+        }
+        result++
+    }
+
+    return result
+};
