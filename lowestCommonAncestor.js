1cfdd4ed1a8cb66b3b100e52e6cf2adfdbcaa2de
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 21:04:03 +0100

    Update lowestCommonAncestor.js

--- lowestCommonAncestor.js
+++ lowestCommonAncestor.js
@@ -1,3 +1,4 @@
+// DFS rekurencyjnie
 var lowestCommonAncestor = function(root, p, q) {
     if (root === null) return null
     if ((p.val <= root.val && q.val >= root.val) || (p.val >= root.val && q.val <= root.val)) return root
@@ -7,4 +8,21 @@ var lowestCommonAncestor = function(root, p, q) {
     } else {
         return lowestCommonAncestor(root.right, p, q)
     }
+};
+
+
+// iteracyjnie
+var lowestCommonAncestor = function(root, p, q) {
+    let curr = root
+
+    while (curr) {
+       if (p.val > cur.val && q.val > cur.val) {
+            cur = cur.right;
+        } else if (p.val < cur.val && q.val < cur.val) {
+            cur = cur.left;
+        } else {
+            return cur;
+        }
+    }
+    return null
 };
