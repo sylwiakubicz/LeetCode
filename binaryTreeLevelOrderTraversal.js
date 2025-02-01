0206eea7dad51c1a9a60778a9d90bb7c2ab358cd
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 21:18:43 +0100

    Update binaryTreeLevelOrderTraversal.js

--- binaryTreeLevelOrderTraversal.js
+++ binaryTreeLevelOrderTraversal.js
@@ -1,1 +1,24 @@
+// bardzo wolne ale działa
+var levelOrder = function(root) {
+    let res = new Array()
+    let queue = [root]
 
+    while (queue.length > 0) {
+        let size = queue.length
+
+        let temp = []
+        for (let i = 0; i < size; i++) {
+            node = queue.pop()
+            if (node !== null) {
+                queue.unshift(node.left)
+                queue.unshift(node.right)
+                temp.push(node.val)
+            }
+        }
+
+        if (temp.length > 0) res.push(temp)
+    }
+
+    return res
+
+};
