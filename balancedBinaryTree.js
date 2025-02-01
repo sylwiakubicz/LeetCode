203d9eb7d4a7fd7281a638c96011c5e4bb24c420
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 15:55:52 +0100

    Update balancedBinaryTree.js

--- balancedBinaryTree.js
+++ balancedBinaryTree.js
@@ -1,1 +1,18 @@
+var isBalanced = function(root) {
+    let res = [true]
+    dfs(root, res)
+    return res[0]
+};
 
+var dfs = function(root, res) {
+    if (root === null) return 0
+
+    let left = dfs(root.left, res)
+    let right = dfs(root.right, res)
+
+    if (Math.abs(right - left) > 1) {
+        res[0] = false
+    }
+
+    return Math.max(left, right) + 1
+}
