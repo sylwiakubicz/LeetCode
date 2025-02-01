ccd8028e17532346e4bd6301bff2908eb4b6b137
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 17:54:55 +0100

    Update subtreeOfAnotherTree.js

--- subtreeOfAnotherTree.js
+++ subtreeOfAnotherTree.js
@@ -1,1 +1,24 @@
+var isSubtree = function(root, subRoot) {
+    if (subRoot === null) return true
+    if (root === null) return false
+    
+    if (isSameTree(root, subRoot)) {
+        return true
+    } else {
+        return isSubtree(root.left, subRoot) ||
+        isSubtree(root.right, subRoot)
+    }
+};
 
+var isSameTree = function(p, q) {
+
+    if (p === null && q === null) {
+        return true
+    }
+    if (p !== null && q !== null && p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(q.right, p.right)
+    } 
+    else {
+        return false
+    }
+};
