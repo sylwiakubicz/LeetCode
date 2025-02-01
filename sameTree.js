0bd4307eb4ee2d2323193809179ed389179b9f05
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 16:26:18 +0100

    Update sameTree.js

--- sameTree.js
+++ sameTree.js
@@ -1,1 +1,25 @@
+// dfs pre-order - moje rozwiązanie
+var isSameTree = function(p, q) {
+    let res = [true]
+    dfs(p,q,res)
+    return res[0]
+};
+
+var dfs = function(p,q,res) {
+    if (p === null || q === null) {
+        if (p !== null || q !== null) {
+            res[0] = false
+        } 
+        return
+    }
+
+    if (p.val !== q.val) {
+        res[0] = false
+    }
+
+    dfs(p.left, q.left, res)
+    dfs(q.right, p.right, res)
+
+    return 
+}
 
