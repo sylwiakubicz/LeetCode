a203255b9b8ed104308f1b3fa97891433ad87be2
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 20:51:09 +0100

    Update lowestCommonAncestor.js

--- lowestCommonAncestor.js
+++ lowestCommonAncestor.js
@@ -1,1 +1,10 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null) return null
+    if ((p.val <= root.val && q.val >= root.val) || (p.val >= root.val && q.val <= root.val)) return root
 
+    if (p.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q)
+    } else {
+        return lowestCommonAncestor(root.right, p, q)
+    }
+};
