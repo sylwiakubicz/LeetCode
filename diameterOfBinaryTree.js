c23efa91f39ba486e08061006664436387407b78
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 10:05:51 +0100

    Update diameterOfBinaryTree.js

--- diameterOfBinaryTree.js
+++ diameterOfBinaryTree.js
@@ -1,1 +1,14 @@
 
+var diameterOfBinaryTree = function(root) {
+    const res = [0]
+    dfs(root,res)
+    return res[0]
+};
+
+var dfs = function(root, res) {
+    if (root === null) return 0
+    let left = dfs(root.left, res)
+    let right = dfs(root.right, res)
+    res[0] = Math.max(res[0], left + right)
+    return Math.max(right, left)  + 1 
+}
