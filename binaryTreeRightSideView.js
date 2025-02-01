3b0c879a1f7921ff4452f2dc0d35240af6b5692c
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 22:02:14 +0100

    Update binaryTreeRightSideView.js

--- binaryTreeRightSideView.js
+++ binaryTreeRightSideView.js
@@ -1,1 +1,22 @@
+var rightSideView = function(root) {
+    let res = new Array()
+    if (root === null) return res
 
+    let q = []
+    q.push(root)
+
+    while (q.length > 0) {
+        let size = q.length
+        let temp = 0
+
+        for (let i = 0; i < size; i++) {
+            let node = q.shift()
+            temp = node.val
+            if (node.left !== null) q.push(node.left)
+            if (node.right !== null) q.push(node.right)
+        }
+        res.push(temp)
+    }
+
+    return res
+};
