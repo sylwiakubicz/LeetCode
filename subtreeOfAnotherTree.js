a695a950ab3e4f74509250f07f38627b7dea2114
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-01 20:20:26 +0100

    Update subtreeOfAnotherTree.js

--- subtreeOfAnotherTree.js
+++ subtreeOfAnotherTree.js
@@ -2,9 +2,14 @@ var isSubtree = function(root, subRoot) {
     if (subRoot === null) return true
     if (root === null) return false
     
+    // sprawdzanie, czy są takie same (jeśli już root się nie zgadza to będzie to tylko jeden if)
     if (isSameTree(root, subRoot)) {
+        // to return pozwala po prostu szybciej zwijać rekurencje 
         return true
     } else {
+        // jeśli pierwsze się okazało że nie jest poddrzewem to przechodzimy po kolejnych nodach z pierwszego drzewka
+        // najpierw po lewej gałęzi dla każdego sprawdzając czy nie jest przypadkiem poddrzewem i dopiero jeśli z lewego mamy false to sprawdzamy prawą
+        // jeśli w lewej stronie było true to dla || nie ma znaczenia co jest po drugiej stronie bo i tak będzie zwrócona prawda więc nawet nei sprawdzamy
         return isSubtree(root.left, subRoot) ||
         isSubtree(root.right, subRoot)
     }
