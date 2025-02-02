5ace2b9a4e667274219f277f9df27b2719fddbc6
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-02 22:55:50 +0100

    Update constructBinaryTree.js

--- tree/constructBinaryTree.js
+++ tree/constructBinaryTree.js
@@ -1,3 +1,25 @@
+// optymalne rozwiązanie do analizy
+var buildTree = function(preorder, inorder) {
+    let preIdx = 0, inIdx = 0;
+    
+        function dfs(limit) {
+            if (preIdx >= preorder.length) return null;
+            if (inorder[inIdx] === limit) {
+                inIdx++;
+                return null;
+            }
+            
+            let root = new TreeNode(preorder[preIdx++]);
+            root.left = dfs(root.val);
+            root.right = dfs(limit);
+            return root;
+        }
+        
+        return dfs(Infinity);
+}
+
+
+// mało optymalne rozwiązanie ale powiedzmy że to rozumiem
 var buildTree = function(preorder, inorder) {
     if (preorder.length === 0 || inorder.length === 0) return null
 
