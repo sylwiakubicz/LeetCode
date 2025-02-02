77600710c49f2818eb0014253b79fb3cc67588b4
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-02 22:42:39 +0100

    Update constructBinaryTree.js

--- tree/constructBinaryTree.js
+++ tree/constructBinaryTree.js
@@ -1,1 +1,10 @@
+var buildTree = function(preorder, inorder) {
+    if (preorder.length === 0 || inorder.length === 0) return null
 
+    let root = new TreeNode(preorder[0])
+    let mid = inorder.indexOf(preorder[0])
+    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0,mid))
+    root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
+    return root
+
+};
