d87ef266e8ddf044238a071c5b7703451e062372
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-02 10:15:57 +0100

    Update countGoodNodesInBinaryTree.js

--- tree/countGoodNodesInBinaryTree.js
+++ tree/countGoodNodesInBinaryTree.js
@@ -1,1 +1,18 @@
+var goodNodes = function(root) {
+    let x = [0]
+    if (root === null) return x[0]
+    return validate(root, root.val, x)
+};
 
+var validate = function(root, min, x) {
+    if (root === null) return 
+
+    if (root.val >= min) {
+        min = root.val
+        x[0]++
+    }
+
+    validate(root.left, min, x)
+    validate(root.right, min, x)
+    return x[0]
+}
