928826bd3abaaed65809b0c987438fd9d7565dc2
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-02 10:25:53 +0100

    Update countGoodNodesInBinaryTree.js

--- tree/countGoodNodesInBinaryTree.js
+++ tree/countGoodNodesInBinaryTree.js
@@ -16,3 +16,20 @@ var validate = function(root, min, x) {
     validate(root.right, min, x)
     return x[0]
 }
+
+
+// neetcode sollution
+goodNodes(root) {
+        return this.dfs(root, root.val);
+    }
+dfs(node, maxVal) {
+        if (!node) {
+            return 0;
+        }
+
+        let res = node.val >= maxVal ? 1 : 0;
+        maxVal = Math.max(maxVal, node.val);
+        res += this.dfs(node.left, maxVal);
+        res += this.dfs(node.right, maxVal);
+        return res;
+    }
