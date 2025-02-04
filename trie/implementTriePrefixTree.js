94ffb650442298a78284d3e87b4842e0b163b88c
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-04 19:30:49 +0100

    Update implementTriePrefixTree.js

--- trie/implementTriePrefixTree.js
+++ trie/implementTriePrefixTree.js
@@ -72,3 +72,79 @@ Trie.prototype.startsWith = function(prefix) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+
+// wersja z hashmapa
+class TrieNode {
+    constructor() {
+        this.children = new Array(26).fill(null);
+        this.endOfWord = false
+    }
+}
+
+var Trie = function() {
+    this.root = new TrieNode()
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let current = this.root
+    for (let char of word) {
+        let index = char.charCodeAt(0) - 97
+        if (current.children[index] === null) {
+            current.children[index] = new TrieNode()
+        }
+        let node = current.children[index]
+        current = node
+    }
+    current.endOfWord = true
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let current = this.root
+    for (let char of word) {
+        let index = char.charCodeAt(0) - 97
+        if (current.children[index] === null) {
+            return false
+        } 
+        let node = current.children[index]
+        current = node
+    }
+
+    if (!current.endOfWord) return false
+
+    return true
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let current = this.root
+    for (let char of prefix) {
+        let index = char.charCodeAt(0) - 97
+        if (current.children[index] === null) {
+            return false
+        } 
+        let node = current.children[index]
+        current = node
+    }
+
+    return true
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
