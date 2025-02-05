79ab7f07d2a95ac685893c045cf1debbb64d60d7
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-05 21:19:17 +0100

    Update subsets.js

--- backtracking/subsets.js
+++ backtracking/subsets.js
@@ -1,1 +1,19 @@
+var subsets = function(nums) {
+    let res = []
+    let subset = []
+    dfs(nums, 0, subset, res)
+    return res
+};
 
+var dfs = function(nums, i, subset, res) {
+    if (i >= nums.length) {
+        res.push([...subset])
+        return
+    }
+    
+    subset.push(nums[i])
+    dfs(nums, i + 1, subset, res)
+    
+    subset.pop()
+    dfs(nums, i + 1, subset, res)
+}
