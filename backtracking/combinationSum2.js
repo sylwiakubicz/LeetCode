252d9dae942f334c76bdc9b7dd5cdc859d4b5bbf
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-06 21:42:19 +0100

    Update combinationSum2.js

--- backtracking/combinationSum2.js
+++ backtracking/combinationSum2.js
@@ -1,1 +1,30 @@
+var combinationSum2 = function(candidates, target) {
+    const nums = candidates.sort((a,b) => a - b)
 
+    let res = []
+    const dfs = (i, current, total) => {
+        if (total === target) {
+            res.push([...current])
+            return
+        }
+
+        
+        if (total + nums[i] > target || i === nums.length) {
+            return
+        }
+
+        current.push(nums[i])
+        dfs(i + 1, current, total + nums[i])
+        current.pop()
+        
+        while (i + 1 < nums.length && nums[i] === nums[i + 1]) {
+            i++;
+        }
+
+        dfs(i + 1, current, total);
+        
+    }
+
+    dfs(0,[],0)
+    return res
+};
