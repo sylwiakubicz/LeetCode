268865e11d2539a4f0a802ab69d746e8200b9ded
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-07 23:11:42 +0100

    Update palindromePartition.js

--- backtracking/palindromePartition.js
+++ backtracking/palindromePartition.js
@@ -1,1 +1,35 @@
+var partition = function(s) {
+    let res = []
+    let subset = []
+    dfs(0, s, [], res)
+    return res
+};
 
+var dfs = function(i, s, subset, res) {
+    if (i >= s.length) {
+        res.push([...subset])
+        return
+    }
+
+    for (let j = i; j < s.length; j++) {
+        if (isPalindrome(s, i, j)) {
+            subset.push(s.substring(i, j + 1))
+            dfs(j + 1, s, subset, res)
+            subset.pop()
+        }
+    }
+
+}
+
+var isPalindrome = function(s, i ,j) {
+   
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+
+    return true    
+}
