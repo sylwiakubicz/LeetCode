c390f4b04adff8abe7ce746bbb5df3046a6ca324
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-07 21:44:08 +0100

    Update permutations.js

--- backtracking/permutations.js
+++ backtracking/permutations.js
@@ -1,3 +1,11 @@
+// zaczynamy od tyłu dla listy [1,2,3]
+// wywołujemy permutate najpierw na 1,2,3 -> potem na 2,3 ---> i na końcu na 3 (co wywołanie ucinamy pierwszy element)
+// zwracamy pusta tablice jako basecae
+// następnie inicjalizujemy res i dodajemy do niego wszystkie permutacje dla ostateniego wywołania czyli dla 3 -> jedna opcaj [3]
+// zwracamy res [[3]] i dla każdej permutacji w perms (czyli w tym co zwróciliśmy w poprzedniej iteracji) 
+// wstawiamy dodatkową liczbę w każde dozwolone miejsce i dodajemy do res 
+// powtarzamy aż wszystkie wywoałania funckji zostaną zamknięte i zwracamy ostateczną liczbę permutacji
+
 var permute = function(nums) {
         if (nums.length === 0) {
             return [[]];
