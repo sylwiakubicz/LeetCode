5c6769653dcc35704c82eb79707e8c128602f5c6
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-07 21:16:58 +0100

    Update permutations.js

--- backtracking/permutations.js
+++ backtracking/permutations.js
@@ -1,1 +1,17 @@
+var permute = function(nums) {
+        if (nums.length === 0) {
+            return [[]];
+        }
+
+        let perms = permute(nums.slice(1));
+        let res = [];
+        for (let p of perms) {
+            for (let i = 0; i <= p.length; i++) {
+                let p_copy = p.slice();
+                p_copy.splice(i, 0, nums[0]);
+                res.push(p_copy);
+            }
+        }
+        return res;
+};
 
