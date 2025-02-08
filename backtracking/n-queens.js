a134d3e1c01546d08f832ef8f2766dca9716af9e
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-08 12:52:54 +0100

    Update n-queens.js'

--- backtracking/n-queens.js
+++ backtracking/n-queens.js
@@ -1,4 +1,5 @@
 var solveNQueens = function(n) {
+    // do śledzenia czy dana kolumn/pozytywna diagonala/negatywna diagonala jest wolna
     let col = new Set()
     let posDiag = new Set()
     let negDiag = new Set()
@@ -18,13 +19,19 @@ var solveNQueens = function(n) {
                     continue;
                 }
 
+                // oznaczamy dane miejsce jako zajęte przez królową więc
+                // w następnym wierszu nie będziemy mogli wstawić w tej samej kolumnie więc ustawiamy ją jako zajętą
+                // nie może też w tej samej pozytywnej diagonali "/" - elementy na tej samej diagonali ich indeksy r i c będą się sumowac do tej samej stałej
+                // nie może byc też na tej samej negatywnej diagonali "\" - elementy na tej samej diagonali,, ich indeksy r i c będą dawały taką samą stałą różnicę
                 col.add(c);
                 posDiag.add(r + c);
                 negDiag.add(r - c);
                 board[r][c] = 'Q';
 
+                // przechodzimy do kolejnego wiersz
                 backtrack(r + 1);
 
+                // gdy się cofamy to musimy zwolnić aktulanie testowaną pozycje królowej - usuwamy zablowaną kolumną, i obie diagonalen z setów oraz ponownie wstawimy "." na planszy
                 col.delete(c);
                 posDiag.delete(r + c);
                 negDiag.delete(r - c);
