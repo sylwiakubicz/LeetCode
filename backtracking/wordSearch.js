9c3f9bfa4466e9dc13c40063a7fcf68d3e8127d3
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-08 11:41:46 +0100

    Update wordSearch.js

--- backtracking/wordSearch.js
+++ backtracking/wordSearch.js
@@ -1,1 +1,30 @@
+var exist = function(board, word) {
+    const ROWS = board.length
+    const COLS = board[0].length
 
+    const visited = new Set()
+    
+    const dfs = (r, c, i) => {
+        if (i === word.length) {
+                return true
+        }
+
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS || board[r][c] !== word[i] || board[r][c] === '#') {
+            return false
+        }
+
+        let temp = board[r][c]
+        board[r][c] = '#';
+        const res = dfs(r + 1, c, i + 1) || dfs(r - 1, c, i + 1) || dfs(r, c + 1, i + 1) || dfs(r, c - 1, i + 1)
+        board[r][c] = temp;
+        return res
+    }
+
+
+    for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLS; c++) {
+                if (board[r][c] === word[0] && dfs(r, c, 0)) return true;
+            }
+    }
+    return false;
+};
