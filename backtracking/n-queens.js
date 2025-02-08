506e179fd7b82b0d8eb44f8172361d3ca9650f5f
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-08 12:45:59 +0100

    Update n-queens.js

--- backtracking/n-queens.js
+++ backtracking/n-queens.js
@@ -1,1 +1,37 @@
+var solveNQueens = function(n) {
+    let col = new Set()
+    let posDiag = new Set()
+    let negDiag = new Set()
 
+    let res = []
+    let board = Array.from({length: n}, () => Array(n).fill('.'));
+    
+    const backtrack = (r) => {
+        if (r === n) {
+            res.push(board.map(row => row.join('')))
+            return
+        }
+
+        for (let c = 0; c < n; c++) {
+                if (col.has(c) || posDiag.has(r + c) ||
+                    negDiag.has(r - c)) {
+                    continue;
+                }
+
+                col.add(c);
+                posDiag.add(r + c);
+                negDiag.add(r - c);
+                board[r][c] = 'Q';
+
+                backtrack(r + 1);
+
+                col.delete(c);
+                posDiag.delete(r + c);
+                negDiag.delete(r - c);
+                board[r][c] = '.';
+        }
+    }
+
+    backtrack(0)
+    return res
+};    
