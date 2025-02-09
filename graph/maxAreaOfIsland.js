8a5bd346c4652830e0f9aaf1b9fe33ca99fa28fa
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-09 15:16:10 +0100

    Update maxAreaOfIsland.js

--- graph/maxAreaOfIsland.js
+++ graph/maxAreaOfIsland.js
@@ -37,5 +37,39 @@ var maxAreaOfIsland = function(grid) {
         }
     }
 
+
+// DFS
+
+    var maxAreaOfIsland = function(grid) {
+    const ROWS = grid.length
+    const COLS = grid[0].length
+    const DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    
+    let maxArea = 0
+    let counter = 0
+
+    const dfs = (r,c) => {
+        if (r < 0 || c < 0 || r >= ROWS || c >= COLS || grid[r][c] === 0) return
+
+        grid[r][c] = 0
+        counter++
+        for (let [dr, dc] of DIRECTIONS) {
+            dfs(r + dr, c + dc)
+        }
+    }
+
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c] === 1) {
+                counter = 0
+                dfs(r,c)
+                maxArea = Math.max(maxArea, counter)
+            }
+        }
+    }
+
+    return maxArea
+};
+
     return maxArea
 };
