59e4233837129b374844ce73e76cfb721514d760
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 19:15:14 +0100

    Update palindrominSubstrings.js

--- twoPointers/palindrominSubstrings.js
+++ twoPointers/palindrominSubstrings.js
@@ -1,1 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let counter = 0
 
+    for (let i = 0; i < s.length; i++) {
+        //odd
+        let l = i
+        let r = i
+        while (l >= 0 && r < s.length && s.charCodeAt(l) === s.charCodeAt(r)) {
+            counter++
+            l--
+            r++
+        }
+
+        //even
+        l = i
+        r = i + 1
+        while (l >= 0 && r < s.length && s.charCodeAt(l) === s.charCodeAt(r)) {
+            counter++
+            l--
+            r++
+        }
+    }
+
+    return counter
+};
