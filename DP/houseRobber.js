ffa259b35f1f6009fadcafa552bac2f2f2759304
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 16:11:33 +0100

    Update houseRobber.js

--- DP/houseRobber.js
+++ DP/houseRobber.js
@@ -1,1 +1,14 @@
+var rob = function(nums) {
+    const N = nums.length
+    let maxMoney = new Array(N + 1)
+    
+    maxMoney[0] = nums[0]
+    maxMoney[1] = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < N; i++) {
+        // aalbo bierzemy 0 i 2 albo tylko 1
+        maxMoney[i] = Math.max(nums[i] + maxMoney[i - 2], maxMoney[i - 1])
+    }
+    
 
+    return maxMoney[N - 1]
+};
