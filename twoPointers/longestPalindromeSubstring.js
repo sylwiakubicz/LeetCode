f4b489caa281c080178f28f9999bc9b0951a193f
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 18:30:40 +0100

    Update longestPalindromeSubstring.js

--- twoPointers/longestPalindromeSubstring.js
+++ twoPointers/longestPalindromeSubstring.js
@@ -1,1 +1,38 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let resIdx = 0
+    let resLen = 0
 
+    for (let i = 0; i < s.length; i++) {
+        //even
+        let l = i
+        let r = i + 1
+
+        while (l >= 0 && r < s.length && s.charCodeAt(l) === s.charCodeAt(r)) {
+            if (r - l + 1 > resLen) {
+                resLen = r - l + 1
+                resIdx = l
+            }
+            l--
+            r++
+        }
+
+        //odd
+        l = i
+        r = i
+        while(l >= 0 && r < s.length && s.charCodeAt(l) === s.charCodeAt(r)) {
+            if (r - l + 1 > resLen) {
+                resLen = r - l + 1
+                resIdx = l
+            }
+            l--
+            r++
+        }
+
+    }
+
+    return s.substring(resIdx, resIdx + resLen)
+};
