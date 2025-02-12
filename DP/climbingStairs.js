0ce1b8c08e2496a9166f705c525ae5d338250ad4
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 14:49:54 +0100

    Update climbingStairs.js

--- DP/climbingStairs.js
+++ DP/climbingStairs.js
@@ -1,1 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let result = new Array(n).fill(0)
 
+    result[1] = 1
+    result[2] = 2
+    for (let i = 3; i <= n; i++) {
+        result[i] = result[i - 2] + result[i - 1]
+    }
+
+    return result[n]
+};
