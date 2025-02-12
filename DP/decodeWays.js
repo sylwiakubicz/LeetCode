cdeecc4da3263a8b33e121d9bd39e5d192a17128
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 20:50:46 +0100

    Update decodeWays.js

--- DP/decodeWays.js
+++ DP/decodeWays.js
@@ -1,1 +1,16 @@
-
+var numDecodings = function(s) {
+    let dp = new Array(s.length + 1).fill(0);
+    dp[s.length] = 1;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s.charAt(i) === '0') {
+             dp[i] = 0;
+        } else {
+            dp[i] = dp[i + 1];
+            if (i + 1 < s.length && (s.charAt(i) === '1' ||
+                (s.charAt(i) === '2' && s.charAt(i + 1) < '7'))) {
+                dp[i] += dp[i + 2];
+            }
+        }
+    }
+    return dp[0];
+};
