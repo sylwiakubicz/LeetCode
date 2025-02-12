9ea2af0da546dfae364e380875e6a4e98cd7beb3
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-12 15:17:29 +0100

    Update minCostClimbingStairs.js

--- DP/minCostClimbingStairs.js
+++ DP/minCostClimbingStairs.js
@@ -1,1 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length 
+    let total = new Array(n)
+    total[n] = 0
+    total[n - 1] = Math.min(cost[n - 1], cost[n - 2])
 
+    for (let i = n - 2; i >= 0; i--){
+        total[i] = cost[i] + Math.min(total[i + 1], total[i + 2])
+    }
+
+    return Math.min(total[0], total[1])
+};
