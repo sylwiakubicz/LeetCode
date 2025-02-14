59775b61d3fca8c302d5200259bd45ce559e0cda
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-14 21:28:34 +0100

    Update wordBreak.js

--- DP/wordBreak.js
+++ DP/wordBreak.js
@@ -1,1 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {    
+    let dp = new Array(s.length + 1).fill(false)
+    dp[s.length] = true
 
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let w of wordDict) {
+            if (i + w.length <= s.length && s.slice(i, i + w.length) === w) {
+                dp[i] = dp[i + w.length]
+            }
+            if (dp[i]) {
+                break
+            }
+        }
+    }
+    return dp[0]
+};
