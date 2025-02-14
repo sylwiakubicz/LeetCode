34f864caaa3e56af2dd8f459aa43c1d27a11f308
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-14 22:40:22 +0100

    Update longestIncreasingSubsequence.js

--- DP/longestIncreasingSubsequence.js
+++ DP/longestIncreasingSubsequence.js
@@ -1,1 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const LIS = new Array(nums.length).fill(1)
 
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                LIS[i] = Math.max(LIS[i], LIS[j] + 1)
+            }
+        }
+    }
+
+    return Math.max(...LIS)
+};
