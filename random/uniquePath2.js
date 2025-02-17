36da717ccec3094f4d30e4aac553d5e94342c070
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-02-17 18:13:16 +0100

    Update uniquePath2.js

--- random/uniquePath2.js
+++ random/uniquePath2.js
@@ -1,3 +1,30 @@
+// dp, przeszło testy ale tylko 24% pokonuje
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    const ROWS = obstacleGrid.length
+    const COLS = obstacleGrid[0].length
+    if (obstacleGrid[0][0] === 1 || obstacleGrid[ROWS - 1][COLS - 1] === 1) return 0
+
+    let dp = Array.from({length: ROWS + 1}, () => Array(COLS + 1).fill(0))
+    dp[ROWS - 1][COLS - 1] = 1
+
+    for (let i = ROWS - 1; i >= 0; i--) {
+        for (let j = COLS -1; j >= 0; j--) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0
+            } else {
+                dp[i][j] += dp[i + 1][j]  
+                dp[i][j] += dp[i][j + 1] 
+            }            
+        }
+    }
+
+    return dp[0][0]
+};
+
 // działa poprawnie ale jest mało optymalne i nie przechodzi testów przez timelimit exceeded
 /**
  * @param {number[][]} obstacleGrid
