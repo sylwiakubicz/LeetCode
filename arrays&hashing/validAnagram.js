a8fa730eb5560f832963e85f0c91a05ba024d4d1
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-04-15 18:33:46 +0200

    Update validAnagram.js

--- arrays&hashing/validAnagram.js
+++ arrays&hashing/validAnagram.js
@@ -39,23 +39,3 @@ var isAnagram = function(s, t) {
     return true
 };
 
-// JAVA SOLUTION
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if (s.length() != t.length()) return false;
-
-//         int[] sLetter = new int[26];
-//         int[] tLetter = new int[26];
-
-//         for (int i = 0; i < s.length(); i++) {
-//             sLetter[Character.codePointAt(s, i) - 97]++;
-//             tLetter[Character.codePointAt(t, i) - 97]++;
-//         }
-
-//         for (int i = 0; i < 26; i++) {
-//             if (sLetter[i] != tLetter[i]) return false;
-//         }
-
-//         return true;
-//     }
-// }
