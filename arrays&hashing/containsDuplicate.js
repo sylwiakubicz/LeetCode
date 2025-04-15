77558a32b9d5d878a2983f2403ad76b2f78ddca4
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-04-15 18:32:50 +0200

    Update containsDuplicate.js

--- arrays&hashing/containsDuplicate.js
+++ arrays&hashing/containsDuplicate.js
@@ -15,19 +15,3 @@ var containsDuplicate = function(nums) {
     return false
 };
 
-
-// JAVA SOLUTION
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         HashSet<Integer> set = new HashSet<>();
-
-//         for (int num:nums) {
-//             if (set.contains(num)) {
-//                 return true;
-//             }
-//             set.add(num);
-//         }
-
-//         return false;
-//     }
-// }
