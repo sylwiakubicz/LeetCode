138b482d7874c543e2e7538aee1b1a12258ea77e
sylwiakubicz <121672102+sylwiakubicz@users.noreply.github.com>
2025-09-18 20:29:21 +0200

    Update RemoveDuplicatesFromSortedArrayII.java

--- arrays&hashing/RemoveDuplicatesFromSortedArrayII.java
+++ arrays&hashing/RemoveDuplicatesFromSortedArrayII.java
@@ -1,5 +1,24 @@
 // first sollution
-
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int uqEl = 0;
+        boolean once = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                if (once) {
+                    once = false;
+                    nums[uqEl] = nums[i];
+                    uqEl++;
+                }
+                continue;
+            }
+            nums[uqEl] = nums[i];
+            uqEl++;
+            once = true;
+        }
+        return uqEl;
+    }
+}
 // more optimal sollution
 class Solution {
     public int removeDuplicates(int[] nums) {
